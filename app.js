ea0f8337c9875452f401b0cf918f9045afddf136
syed shaida <syedshaida295@gmail.com>
2021-08-02 13:20:12 +0530

    app done

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+//   var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+//  var serverURL = "https://api.funtranslations.com/translate/groot.json"
+//  var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(input) {
+        return serverURL + "?" + "text = " + input;
+}
+
+function errorHandler(error) {
+        console.log("error occured " + error);
+        alert("something wrong with the server, try again after some time.")
+}
+
+function clickHandler() {
+        var inputText = txtInput.value;
+        fetch(getTranslationURL(inputText))
+                .then(response => response.json())
+                .then(json => {
+                        var translatedText = json.contents.translated;
+                        outputDiv.innerHTML = translatedText;
+                })
+                .catch(errorHandler);
+};
 
 btnTranslate.addEventListener("click", clickHandler);
-
-    function clickHandler() {
-            console.log("Clicked!");
-            console.log(txtInput.value);
-    }
-
